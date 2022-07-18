7468a19ba7079053ee00f8038d78c4ba9ed39e85
isibol98 <ilkkanisibol@gmail.com>
2022-07-18 16:45:50 +0300

    Improvments

--- SchoolDb/schooldb-demo.py
+++ SchoolDb/schooldb-demo.py
@@ -3,6 +3,21 @@ from mysql.connector import Error
 from datetime import datetime
 import time
 
+def filter_ (a) -> str: # a -> Search/Update/Delete
+    filter = input(f"{a} ...\n1-Student Number\n2-Student Name\n3-Student Surname\n4-Student Gender\n")
+    if filter == "1":
+        filter = "studentnumber"
+    elif filter == "2":
+        filter = "name"
+    elif filter == "3":
+        filter = "surname"
+    elif filter == "4":
+        filter = "Gender"
+    else:
+        print("Try again.")
+        filter_()
+    return filter
+
 class Student:
     mydb = mydb
     mycursor = mydb.cursor()
@@ -39,8 +54,8 @@ class Student:
         for student in result:
             print(student)
 
-    def updateStudent(self): #Add in choices?
-        s.mycursor.execute("Update Student Set {newFilter}='{newInfo}' Where {filter}={search}") #... Set name='Ilkkan' Where name='Ali'
+    def updateStudent(self,newFilter,newSearch,filter,search): 
+        s.mycursor.execute(f"Update Student Set {newFilter}='{newSearch}' Where {filter}={search}") #... Set name='Ilkkan' Where name='Ali'
         try:
             s.mydb.commit()
             print(f"{s.mycursor.rowcount} student(s) updated!")
@@ -50,8 +65,8 @@ class Student:
         finally:
             s.mydb.close()
             print("Database closed!")
-    def deleteStudent (self): #Add in choices?
-        s.mycursor.execute("Delete From Student Where id=1") #Change this later
+    def deleteStudent (self,filter,search): 
+        s.mycursor.execute(f"Delete From Student Where {filter}={search}")
         try:
             s.mydb.commit()
             print(f"{s.mycursor.rowcount} student(s) deleted!")
@@ -65,7 +80,7 @@ class Student:
 students = []
 s = Student(students)
 
-choice = input("Welcome! What you want to do?\n1-Add New Student\n2-Check Students\n3-Search a Student\n4-Exit\n:")
+choice = input("Welcome! What you want to do?\n1-Add New Student\n2-Check Students\n3-Search a Student\n4-Update a Student's Data\n5-Delete a Student\n6-Exit\n:")
 if choice == "1":
     while True:
         studentNumber = input("Student Number: ")
@@ -88,21 +103,28 @@ if choice == "1":
             break
 elif choice == "2":
     s.getStudents()
-elif choice == "3": #Create func for filter?
-    filter = input("Search with ...\n1-Student Number\n2-Student Name\n3-Student Surname\n4-Student Gender\n")
-    if filter == "1":
-        filter = "studentnumber"
-    elif filter == "2":
-        filter = "name"
-    elif filter == "3":
-        filter = "surname"
-    else:
-        filter = "Gender"
+elif choice == "3":
+    filter = filter_("Search")
     search = input(f"Enter Student {filter}:\n")
     s.searchStudents(filter, search)
+elif choice == "4":
+    newFilter = filter_("Update")
+    filter = filter_("Find")
+    newSearch = input(f"Enter the New {newFilter} Value: ")
+    search = input(f"Enter Student {filter}:\n")
+    s.updateStudent(newFilter,newSearch,filter,search)
+elif choice == "5":
+    filter = filter_("Delete")
+    search = input(f"Enter Student {filter}:\n")
+    s.deleteStudent(filter,search)
+elif choice == "6":
+    print("Shutting down...")
+    time.sleep(2)
+    quit()
 else:
-    print("Good bye...")
-    time.sleep(4)
+    print("Try again.")
+
+    
 
 
 """"
